afacc0c76b2f9b846dad09b2b87759c6f2117873
mariiadidenko11 <mariia.didenko1@nure.ua>
2026-05-30 22:44:05 +0200

    Delete src directory

--- src/components/ui/ProgressBar.jsx
+++
@@ -1,9 +0,0 @@
-export default function ProgressBar({ value = 0, variant = '', thin = false }) {
-    const cls = ['progress', variant && `progress--${variant}`, thin && 'progress--thin']
-        .filter(Boolean).join(' ');
-    return (
-        <div className={cls}>
-            <div className="progress__fill" style={{ width: `${Math.min(100, value)}%` }} />
-        </div>
-    );
-}
